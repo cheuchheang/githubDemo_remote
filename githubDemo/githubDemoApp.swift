684fa4853385f56bdb6c8f4a72b22f82aede9386
searcheuchheang <searcheuchheang@gmail.com>
2022-11-20 15:47:39 +0700

    Initial Commit

---
+++ githubDemo/githubDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubDemoApp.swift
+//  githubDemo
+//
+//  Created by Macintosh on 20/11/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct githubDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
