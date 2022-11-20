d62d4c80cf185ea03b5245da5fb6510fb2ddc294
searcheuchheang <searcheuchheang@gmail.com>
2022-11-20 16:00:37 +0700

    hello earth

---
+++ githubDemo/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  githubDemo
+//
+//  Created by Macintosh on 20/11/2565 BE.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text("hello earth")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
